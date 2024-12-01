f3e2868a8f5f6978f33ac136f388004a667e6a4b
Your Name <s.r.j.s.n2017@gmail.com>
2024-12-01 10:02:34 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { decreaseCounter, increamentCounter } from "../utils/counterSlice";
 
 const App = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button onClick={() => dispatch(decreaseCounter())}>-</button>
+        <span>{counter}</span>
+        <button onClick={() => dispatch(increamentCounter())}>+</button>
     </div>
   )
 }
